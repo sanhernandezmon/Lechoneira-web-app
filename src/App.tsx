8712d4b19dd72b9460c711c61dd8f0cd99200c68
Liz <elizabethmontanorodriguez@gmail.com>
2024-12-28 19:35:20 -0500

    Incorporando plantilla twin-love

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,17 @@ import Topbar from "./components/Topbar";
 import { ChakraProvider } from "@chakra-ui/react";
 import DressingCode from "./components/DressingCode";
 import DayCounter from "./components/DayCounter";
+import Navbar from "./components/Navbar";
+import './App.css';
+import './css/font-awesome.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const App = () => {
   return (
     <ChakraProvider>
       <div>
-        <Topbar />
-        <DayCounter />
-        <Gallery />
-        <DressingCode />
+        <Navbar />
       </div>
     </ChakraProvider>
   );
