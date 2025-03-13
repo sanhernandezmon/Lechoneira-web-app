284489f4caa41e80715ceec2d9ceaefc37f6bc26
Santiago Hernandez <elsantimontano@gmail.com>
2025-03-12 21:37:16 -0500

    ci :rocket: deploy add naviagte

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 import "./App.css";
 import "./css/font-awesome.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import DashBoardPage from "./pages/DashboardPage";
 import InventaryPage from "./pages/InventaryPage";
 import SellsPage from "./pages/SellsPage";
@@ -13,6 +13,7 @@ const App = () => {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<DashBoardPage />} />
+          <Route path="/Lechoneira-web-app/" element={<Navigate to="/" />} />
           <Route path="/inventario" element={<InventaryPage />} />
           <Route path="/ventas" element={<SellsPage />} />
         </Routes>
