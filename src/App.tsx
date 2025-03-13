77bf67ebd50bfdc26eb090abe181de40445a7cac
Santiago Hernandez <elsantimontano@gmail.com>
2025-03-12 21:41:48 -0500

    ci :rocket: deploy naviagate fixed

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<DashBoardPage />} />
-          <Route path="/Lechoneira-web-app/" element={<Navigate to="/" />} />
+          <Route path="/Lechoneira-web-app" element={<Navigate to="/" />} />
           <Route path="/inventario" element={<InventaryPage />} />
           <Route path="/ventas" element={<SellsPage />} />
         </Routes>
